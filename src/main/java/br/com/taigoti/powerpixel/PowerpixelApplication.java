a0588651a014ed4c392d7262325d971dbb4ad3f2
taigoti <taigoti@proton.me>
2026-07-06 21:55:40 -0300

    spring boot init

---
+++ src/main/java/br/com/taigoti/powerpixel/PowerpixelApplication.java
@@ -0,0 +1,13 @@
+package br.com.taigoti.powerpixel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerpixelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerpixelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/taigoti/powerpixel/PowerpixelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taigoti.powerpixel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerpixelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
