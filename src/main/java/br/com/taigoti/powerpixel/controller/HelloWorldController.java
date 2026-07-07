99a336f6bdb3f009d231230109ec5f9d5dd76b2b
taigoti <taigoti@proton.me>
2026-07-06 22:53:44 -0300

    adicionado RequestParam, PathVariables e RequestBody nas requisições

--- src/main/java/br/com/taigoti/powerpixel/controller/HelloWorldController.java
+++ src/main/java/br/com/taigoti/powerpixel/controller/HelloWorldController.java
@@ -7,15 +7,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/v1/hello")
 public class HelloWorldController {
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public String helloWorldGET() {
-        return "getting the Hello World!";
+    //GET por meio de endpoint '/v1/hello/{id}
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)  //HTTP 200
+    public String getWithId(@PathVariable("id") String id) {
+        return "getting the Hello World with " + id + "!";
     }
 
+    //GET por meio de parâmetros
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)  //HTTP 200
+    public String getDefault(@RequestParam(value = "name", required = false) String name) {
+        return "getting the Hello World! without id " + name;
+    }
+
+    //POST com conteúdo no body
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public String helloWorldPOST() {
-        return "posting the Hello World!";
+    @ResponseStatus(HttpStatus.CREATED)  //HTTP 201
+    public String helloWorldPOST(@RequestBody String name) {
+        return "posting the Hello World!, " + name;
     }
 }
