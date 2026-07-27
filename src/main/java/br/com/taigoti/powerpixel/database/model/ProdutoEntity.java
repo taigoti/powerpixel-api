255d7cffbac8c08744ec6091b673c8d0eb327f55
taigoti <taigoti@proton.me>
2026-07-26 21:10:51 -0300

    feat: Entities criadas

--- src/main/java/br/com/taigoti/powerpixel/database/model/ProdutoEntity.java
+++ src/main/java/br/com/taigoti/powerpixel/database/model/ProdutoEntity.java
@@ -1,17 +1,30 @@
 package br.com.taigoti.powerpixel.database.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
+@Entity
+@Table (name = "produtos")
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@ToString
-@Builder
+@AllArgsConstructor
 public class ProdutoEntity {
 
-    private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String nome;
-    private Double preco;
-    private Integer quantidade;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private String estoque;
+
+    private String sku;
 }
---
+++ src/main/java/br/com/taigoti/powerpixel/database/model/UsuarioEntity.java
@@ -0,0 +1,31 @@
+package br.com.taigoti.powerpixel.database.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false,  unique = true)
+    private String email;
+
+    @Column(name = "data_criacao",  nullable = false)
+    private Date dataCriacao;
+}
