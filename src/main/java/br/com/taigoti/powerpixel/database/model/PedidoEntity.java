71eed641ead7d223c5a719c61bba65f37aed9f24
taigoti <taigoti@proton.me>
2026-07-26 23:42:04 -0300

    fix: application.yaml e pom.xml adaptados

--- src/main/java/br/com/taigoti/powerpixel/database/model/PedidoEntity.java
+++ src/main/java/br/com/taigoti/powerpixel/database/model/PedidoEntity.java
@@ -1,10 +1,7 @@
 package br.com.taigoti.powerpixel.database.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "pedidos")
