3ba641836a7c04f9e74789e18103f249f887ab58
Tiago Igor <taigoti@proton.me>
2026-08-12 07:20:19 -0300

    feat: adiciona relacionamentos 1:1/1:N em pedidos

--- src/main/java/br/com/taigoti/powerpixel/database/model/PedidoEntity.java
+++ src/main/java/br/com/taigoti/powerpixel/database/model/PedidoEntity.java
@@ -2,8 +2,14 @@ package br.com.taigoti.powerpixel.database.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "pedidos")
 @Getter
 @Setter
@@ -14,4 +20,17 @@ public class PedidoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany
+    @JoinColumn(name = "produtos")
+    private List<ProdutoEntity> produtos;
+
+    @OneToOne
+    @JoinColumn(name = "usuarios")
+    private UsuarioEntity usuario;
+
+    @CreatedDate
+    @Column(name = "data_criacao",  nullable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+
 }
