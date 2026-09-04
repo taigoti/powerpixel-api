7b2323873d84204eaf0d354c213f1c3af13cba67
Tiago Igor <taigoti@proton.me>
2026-09-04 00:02:56 -0300

    feat: validação de POST com @Valid

--- src/main/java/br/com/taigoti/powerpixel/controller/ProdutoController.java
+++ src/main/java/br/com/taigoti/powerpixel/controller/ProdutoController.java
@@ -3,8 +3,10 @@ package br.com.taigoti.powerpixel.controller;
 import br.com.taigoti.powerpixel.database.model.ProdutoEntity;
 import br.com.taigoti.powerpixel.dto.ProdutoDto;
 import br.com.taigoti.powerpixel.service.ProdutoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/produtos")
 @RequiredArgsConstructor
+@Validated
 public class ProdutoController {
 
     private final ProdutoService produtoService;
@@ -24,7 +27,7 @@ public class ProdutoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void saveProduto(@RequestBody ProdutoDto produtoDto) {
+    public void saveProduto(@Valid @RequestBody ProdutoDto produtoDto) {
         produtoService.saveProduto(produtoDto);
     }
 }
--- src/main/java/br/com/taigoti/powerpixel/dto/ProdutoDto.java
+++ src/main/java/br/com/taigoti/powerpixel/dto/ProdutoDto.java
@@ -1,5 +1,7 @@
 package br.com.taigoti.powerpixel.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -12,8 +14,12 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class ProdutoDto {
 
+    @NotBlank
     public String nome;
+    @NotNull
     public BigDecimal preco;
+    @NotNull
     public Integer estoque;
+    @NotBlank
     public String sku;
 }
