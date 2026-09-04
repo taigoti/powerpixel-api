582a55866600606ddd29a9982d758c643763c591
Tiago Igor <taigoti@proton.me>
2026-09-03 23:57:02 -0300

    feat: criando método POST para produto

--- src/main/java/br/com/taigoti/powerpixel/controller/ProdutoController.java
+++ src/main/java/br/com/taigoti/powerpixel/controller/ProdutoController.java
@@ -1,13 +1,11 @@
 package br.com.taigoti.powerpixel.controller;
 
 import br.com.taigoti.powerpixel.database.model.ProdutoEntity;
+import br.com.taigoti.powerpixel.dto.ProdutoDto;
 import br.com.taigoti.powerpixel.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,11 @@ public class ProdutoController {
     @ResponseStatus(HttpStatus.OK)
     public List<ProdutoEntity> findAll() {
         return produtoService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveProduto(@RequestBody ProdutoDto produtoDto) {
+        produtoService.saveProduto(produtoDto);
     }
 }
