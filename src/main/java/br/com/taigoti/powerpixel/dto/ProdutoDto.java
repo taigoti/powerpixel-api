4b91ac499671725d3fdbe8d06bfad4190dd4e275
Tiago Igor <taigoti@proton.me>
2026-09-03 23:42:41 -0300

    feat: cria DTO para produtos

---
+++ src/main/java/br/com/taigoti/powerpixel/dto/ProdutoDto.java
@@ -0,0 +1,18 @@
+package br.com.taigoti.powerpixel.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoDto {
+
+    public String nome;
+    public BigDecimal preco;
+    public Integer estoque;
+}
